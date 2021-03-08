21a45e836b8555fe763781b3864494586df27836
yo-mensch <rasa.j1225@gmail.com>
2021-03-08 12:41:14 +0200

    Add header color

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
 const express = require("express");
-//const bodyParser = require("body-parser");
 const https = require("https");
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 
 app.get("/", function(req,res){
     var today = new Date();
-    var day="";
-    day = new Intl.DateTimeFormat('en-US',{ weekday: 'long'}).format(today);
+    var day = new Intl.DateTimeFormat('en-US',{ weekday: 'long'}).format(today);
     res.render('list', {kindOfDay: day});
 })
 
