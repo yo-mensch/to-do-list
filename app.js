692a4a90254a7587da31e4f8f264a01f080ff54b
Rasa Jurgutytė <53710574+yo-mensch@users.noreply.github.com>
2021-04-24 23:00:40 +0300

    Import mongoose

--- app.js
+++ app.js
@@ -1,18 +1,51 @@
 const express = require("express");
 const date = require("./date");
+const mongoose = require("mongoose");
 
 const app = express();
 
-let items = [];
-let workItems=[];
+//let items = [];
+//let workItems=[];
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 
+mongoose.connect("mongodb://localhost:27017/toDoListDB",{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+const listItemSchema = new mongoose.Schema({
+    content: String
+})
+
+const Item = new mongoose.model("item",listItemSchema);
+//default items
+/*const item1 = new Item({
+    content: "Udemy kursus pribaigineti"
+})
+const item2 = new Item({
+    content: "Sobolio egzui pasimokyti"
+})
+
+const defaultItems = [item1, item2];*/
+/*Item.insertMany(defaultItems,(err)=>{
+    if(err){
+        console.log(err);
+    } else {
+        console.log("success!");
+    }
+})*/
+
 app.get("/", function(req,res){
-    res.render('list', {listTitle: date.getDate(), newTask: items});
+    Item.find((err, foundItems)=>{
+        if(err) console.log(err);
+        else {
+            res.render('list', {listTitle: date.getDate(), newTask: foundItems});
+        }
+    })
 });
 
 app.get("/work", (req,res)=>{
